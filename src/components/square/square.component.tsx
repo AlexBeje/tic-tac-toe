eb5f86f8f6873f814882725b03bdf2d8abe8a023
Alex <alex.bejenaru93@gmail.com>
2020-11-13 18:52:03 +0100

    📦 NEW: Added on click event example.

--- src/components/square/square.component.tsx
+++ src/components/square/square.component.tsx
@@ -5,12 +5,26 @@ import "./square.styles.css";
 
 // Type definition
 type SquareProps = {
-  value: number;
+  value: number | string | null;
 };
 
-class Square extends React.Component<SquareProps> {
+type SquareState = {
+  value: number | string | null;
+};
+
+class Square extends React.Component<SquareProps, SquareState> {
+  constructor(props: SquareState) {
+    super(props);
+    this.state = {
+      value: null,
+    };
+  }
   render() {
-    return <div className="square">{this.props.value}</div>;
+    return (
+      <div className="square" onClick={() => this.setState({ value: "X" })}>
+        {this.state.value}
+      </div>
+    );
   }
 }
 
